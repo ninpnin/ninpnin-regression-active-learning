cd9e7671d88e82adbdc6a8ae63ccac02461f19c9
ninpnin <vainoyrjanainen@icloud.com>
2022-05-23 17:55:49 +0200

    fix: randomize independent variable instead of providing a range

--- generate_data.py
+++ generate_data.py
@@ -44,7 +44,9 @@ def leaky_sine(x, threshold=1.0, a=2.0, b=0.1, sines=2):
 
 def main(args):
     # Generate data
-    x = tf.range(-200.0, 200.0) / 50.0
+    x = (tf.random.uniform([args.n]) - 0.5) * 8.0
+    print(x)
+
     mu = tf.map_fn(leaky_sine, x)
     y = tf.random.normal(shape=[len(mu)],mean=0.0, stddev=args.sigma) + mu
 
@@ -66,6 +68,7 @@ def main(args):
 if __name__ == '__main__':
     import argparse                                                                                                   
     parser = argparse.ArgumentParser()                                                                                
+    parser.add_argument("--n", type=float, default=400)
     parser.add_argument("--sigma", type=float, default=1.0)
     args = parser.parse_args()
 
